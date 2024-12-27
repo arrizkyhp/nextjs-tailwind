6c8bc9ea8445456a48ca93a56c62fbf99a4a8e74
arrizky hasya pratama <arrizkyhasya@gmail.com>
2024-12-27 23:18:43 +0700

    feat: add shadcn & add button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Image from "next/image";
+import {Button} from "@@/components/ui/button";
 
 export default function Home() {
   return (
@@ -47,6 +50,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button variant="outline" onClick={() => console.log("AAA")}>Click me</Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
