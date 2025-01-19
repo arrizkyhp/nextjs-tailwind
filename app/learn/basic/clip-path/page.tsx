2d704c0090fc085bc9463a0d76a7a9f7c039ea3a
arrizky hasya pratama <arrizkyhasya@gmail.com>
2025-01-19 10:06:37 +0700

    feat: add clippath menu

---
+++ app/learn/basic/clip-path/page.tsx
@@ -0,0 +1,12 @@
+import ClipPathView from "@@/features/clip-path/ClipPath/ClipPath";
+
+const ClipPathPage = () => {
+
+    return (
+        <>
+           <ClipPathView />
+        </>
+    )
+}
+
+export default ClipPathPage;
